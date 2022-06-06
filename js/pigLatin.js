68f1b0be687cbe2a8d63b1fda21e672a9ff6ffd4
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-06 14:21:42 -0500

    SOLVED

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,24 @@
 exports.translate = function(word) {
+    let splitter = word.split(' ')
+    let finalSentence = []
+    let vowels = /[a, e, i , o, u, w, y]/i
+    let upper = /[A-Z]/
+
+    for(let i = 0; i < splitter.length; i++){
+        splitter[i] = splitter[i].split('')
+        while(vowels.test(splitter[i][0]) != true){
+            if(splitter[i][0] == 'q'){
+                splitter[i].push(splitter[i].shift())
+                splitter[i].push(splitter[i].shift())
+            }
+            else{
+            splitter[i].push(splitter[i].shift())
+            }
+        }
+        splitter[i].push('ay')
+        splitter[i] = splitter[i].join('')
+        finalSentence.push(splitter[i])
+    }
+    finalSentence = finalSentence.join(' ')
+    return finalSentence
 };
--- python/pig_latin.py
+++ python/pig_latin.py
@@ -1,3 +1,23 @@
+import re
+
 def translate(word_or_phrase):
-  pass
+  listed = word_or_phrase.split()
+  final_phrase = []
+  for i in listed:
+    seek = re.search('[aeiou]', i[0])
+    i = list(i)
+    while seek == None:
+      if i[0] == 'q':
+        i.append(i.pop(0))
+        i.append(i.pop(0))
+        seek = re.search('[aeiou]', i[0])
+      else:
+        i.append(i.pop(0))
+        seek = re.search('[aeiou]', i[0])
+    i.append('ay')
+    final_phrase.append(''.join(i))
+
+  return ' '.join(final_phrase)
+
+print(translate('the quick brown fox'))
 
